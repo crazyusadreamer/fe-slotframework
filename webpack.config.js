95d08ba9fd18808257d1453e3796745d2647097b
Artem Fedorov <artemfedorov@Artems-MacBook-Pro.local>
2023-02-27 13:32:27 -0500

    changed to localhost

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require("path");
-const { EnvironmentPlugin } = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const resolveTsconfigPathsToAlias = require("./resolve-tsconfig-path-to-webpack-alias");
+const path = require('path')
+const { EnvironmentPlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const resolveTsconfigPathsToAlias = require('./resolve-tsconfig-path-to-webpack-alias')
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production'
 
-const stylesHandler = MiniCssExtractPlugin.loader;
-
+const stylesHandler = MiniCssExtractPlugin.loader
 
 const config = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     open: true,
-    host: "0.0.0.0",
+    host: 'localhost',
     port: 3000,
     compress: true,
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "src/assets", to: "./assets" }],
+      patterns: [{ from: 'src/assets', to: './assets' }],
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
 
     new MiniCssExtractPlugin(),
@@ -37,16 +36,16 @@ const config = {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
@@ -55,26 +54,26 @@ const config = {
   },
   resolve: {
     alias: resolveTsconfigPathsToAlias({
-      tsconfigPath: "./tsconfig.json",
-      webpackConfigBasePath: "./",
+      tsconfigPath: './tsconfig.json',
+      webpackConfigBasePath: './',
     }),
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
-};
+}
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
-    config.devtool = "inline-source-map";
-    config.output.filename = "index.js";
+    config.mode = 'production'
+    config.devtool = 'inline-source-map'
+    config.output.filename = 'index.js'
     config.optimization = {
       minimize: false,
-    };
+    }
   } else {
-    config.mode = "development";
-    config.devtool = "inline-source-map";
-    config.output.filename = "index.js";
+    config.mode = 'development'
+    config.devtool = 'inline-source-map'
+    config.output.filename = 'index.js'
   }
 
-  return config;
-};
+  return config
+}
